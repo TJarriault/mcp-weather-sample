e8b175edc00eb7c66098514bb6247aceadbbdb85
tony jarriault <tony.jarriault@capgemini.com>
2025-11-06 11:38:12 +0100

    update weather on minikube

--- test-keycloak.sh
+++ test-keycloak.sh
@@ -12,11 +12,17 @@ OAUTH_CLIENT_SECRET=HFLjyBhlfn2WgQZWVBZ5ASItetLL04iW
 
 echo $OAUTH_CLIENT_ID
 
-curl -X POST -k \
+TOKEN=$(curl -X POST -k \
   -d "client_id=${OAUTH_CLIENT_ID}" \
   -d "client_secret=${OAUTH_CLIENT_SECRET}" \
   -d "grant_type=client_credentials" \
-  "https://${AUTH_HOST}/realms/master/protocol/openid-connect/token"
+  "https://${AUTH_HOST}/realms/master/protocol/openid-connect/token" \
+  | jq -r .access_token)
 
+echo "Step 2"
+
+curl -H "Authorization: Bearer $TOKEN" \
+     -H "Content-Type: application/json" \
+     https://weath-auth.192.168.49.2.nip.io/ -k
 
 
